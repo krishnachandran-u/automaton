44dcd1a3c8878f91933dfc54c0af325a8ac10a7c
Krishnachandran U <kriss52450@gmail.com>
2024-11-15 23:54:27 +0530

    create nfa from postfix of the regex

--- nfa.py
+++ nfa.py
@@ -62,3 +62,58 @@ def regexTree_to_nfa(tree: TreeNode, cnt: int = 0) -> tuple[Dict[str, str | List
 
     else:
         raise ValueError(f"Invalid operator {tree.value}")
+
+def postfix_to_nfa(postfix: str) -> Dict[str, str | List[str] | List[List[str]]]: 
+    stack: List[Dict[str, str | List[str] | List[List[str]]]] = []
+    operators = ['+', '.', '*']
+    cnt: int = 0
+    for char in postfix: 
+        if char not in operators:
+            new_nfa = {
+                'name': f"<{postfix}>postfix_to_nfa",
+                'states': [f'p{cnt}', f'p{cnt + 1}'],
+                'alphabet': [char] if char not in ['ε', 'φ'] else [],
+                'transitions': [[f'p{cnt}', char, f'p{cnt + 1}']] if char != 'φ' else [],
+                'start_state': f'p{cnt}',
+                'final_states': [f'p{cnt + 1}'],
+            }
+            cnt += 2
+            stack.append(new_nfa)
+        else:
+            if char == '*':
+                left_nfa = stack.pop()
+                new_nfa = deepcopy(left_nfa)
+                new_nfa['transitions'].append([left_nfa['start_state'], 'ε', left_nfa['final_states'][0]])
+                new_nfa['transitions'].append([left_nfa['final_states'][0], 'ε', left_nfa['start_state']])
+            elif char == '+':
+                right_nfa = stack.pop()
+                left_nfa = stack.pop()
+                new_nfa = {
+                   'name': f"<{postfix}>postfix_to_nfa",
+                   'states': left_nfa['states'] + right_nfa['states'],
+                   'alphabet': list(set(left_nfa['alphabet'] + right_nfa['alphabet'])),
+                   'transitions': left_nfa['transitions'] + right_nfa['transitions'],
+                   'start_state':  left_nfa['start_state'], 
+                   'final_states': right_nfa['final_states']
+                }
+        
+                new_nfa['transitions'].append([left_nfa['start_state'], 'ε', right_nfa['start_state']])
+                new_nfa['transitions'].append([right_nfa['final_states'][0], 'ε', right_nfa['final_states'][0]])
+
+            elif char == '.':
+                right_nfa = stack.pop()
+                left_nfa = stack.pop()
+                new_nfa = {
+                    'name': f"<{postfix}>postfix_to_nfa",
+                    'states': left_nfa['states'] + right_nfa['states'],
+                    'alphabet': list(set(left_nfa['alphabet'] + right_nfa['alphabet'])),
+                    'transitions': left_nfa['transitions'] + right_nfa['transitions'],
+                    'start_state':  left_nfa['start_state'], 
+                    'final_states': right_nfa['final_states']
+                }
+                new_nfa['transitions'].append([left_nfa['final_states'][0], 'ε', right_nfa['start_state']])
+
+            else:
+                raise ValueError(f"Invalid operator {tree.value}")
+            stack.append(new_nfa) 
+    return stack.pop()
