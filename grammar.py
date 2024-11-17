8743580e0b4fb3c785c5087d000f7c4c6d1f3e0e
Krishnachandran U <kriss52450@gmail.com>
2024-11-17 10:14:53 +0530

    refactor: rename new_state to next_state for clarity

--- grammar.py
+++ grammar.py
@@ -16,32 +16,32 @@ def rlg_to_nfa(rlg: Dict[str, Dict[str, List[str]] | List[str]]) -> Dict[str, st
             if production == 'ε':
                 nfa['final_states'].append(symbol) if symbol not in nfa['final_states'] else None
             elif production in rlg['terminals']:
-                new_state = f'p{cnt}'
+                next_state = f'p{cnt}'
                 cnt += 1
-                nfa['states'].append(new_state)
-                nfa = add_transition(nfa, symbol, production, new_state)
-                nfa['final_states'].append(new_state)
+                nfa['states'].append(next_state)
+                nfa = add_transition(nfa, symbol, production, next_state)
+                nfa['final_states'].append(next_state)
             elif production in rlg['non_terminals']:
-                new_state = production
-                nfa = add_transition(nfa, symbol, 'ε', production)
+                next_state = production
+                nfa = add_transition(nfa, symbol, 'ε', next_state)
             else:
                 curr_state = symbol
                 for i, char in enumerate(production):
                     if i == len(production) - 2 and production[i + 1] in rlg['non_terminals']:
-                        new_state = production[i + 1]
-                        nfa['states'].append(new_state) if new_state not in nfa['states'] else None
-                        nfa = add_transition(nfa, curr_state, char, new_state)
+                        next_state = production[i + 1]
+                        nfa['states'].append(next_state) if next_state not in nfa['states'] else None
+                        nfa = add_transition(nfa, curr_state, char, next_state)
                         break
                     if i == len(production) - 1: # implied that production[i] is a terminal
-                        new_state = f'p{cnt}'
+                        next_state = f'p{cnt}'
                         cnt += 1
-                        nfa['states'].append(new_state)
-                        nfa = add_transition(nfa, curr_state, char, new_state)
-                        nfa['final_states'].append(new_state)
+                        nfa['states'].append(next_state)
+                        nfa = add_transition(nfa, curr_state, char, next_state)
+                        nfa['final_states'].append(next_state)
                     else:
-                        new_state = f'p{cnt}'
+                        next_state = f'p{cnt}'
                         cnt += 1
-                        nfa['states'].append(new_state)
-                        nfa = add_transition(nfa, curr_state, char, new_state)
-                        curr_state = new_state
+                        nfa['states'].append(next_state)
+                        nfa = add_transition(nfa, curr_state, char, next_state)
+                        curr_state = next_state
     return nfa
