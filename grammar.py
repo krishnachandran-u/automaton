731a3a4a19c1d8f84724007d45a538e0eb50d529
Krishnachandran U <kriss52450@gmail.com>
2024-11-17 10:11:49 +0530

    fix: remove unnecessary epslion transitions in rlg to nfa

--- grammar.py
+++ grammar.py
@@ -15,19 +15,29 @@ def rlg_to_nfa(rlg: Dict[str, Dict[str, List[str]] | List[str]]) -> Dict[str, st
         for production in productions:
             if production == 'ε':
                 nfa['final_states'].append(symbol) if symbol not in nfa['final_states'] else None
+            elif production in rlg['terminals']:
+                new_state = f'p{cnt}'
+                cnt += 1
+                nfa['states'].append(new_state)
+                nfa = add_transition(nfa, symbol, production, new_state)
+                nfa['final_states'].append(new_state)
+            elif production in rlg['non_terminals']:
+                new_state = production
+                nfa = add_transition(nfa, symbol, 'ε', production)
             else:
                 curr_state = symbol
                 for i, char in enumerate(production):
-                    if i == len(production) - 1:
-                        if char in rlg['terminals']:
-                            new_state = f'p{cnt}'
-                            cnt += 1
-                            nfa['states'].append(new_state)
-                            nfa = add_transition(nfa, curr_state, char, new_state)
-                            nfa['final_states'].append(new_state)
-                        if char in rlg['non_terminals']:
-                            nfa = add_transition(nfa, curr_state, 'ε', char)
-                            nfa['states'].append(char) if char not in nfa['states'] else None
+                    if i == len(production) - 2 and production[i + 1] in rlg['non_terminals']:
+                        new_state = production[i + 1]
+                        nfa['states'].append(new_state) if new_state not in nfa['states'] else None
+                        nfa = add_transition(nfa, curr_state, char, new_state)
+                        break
+                    if i == len(production) - 1: # implied that production[i] is a terminal
+                        new_state = f'p{cnt}'
+                        cnt += 1
+                        nfa['states'].append(new_state)
+                        nfa = add_transition(nfa, curr_state, char, new_state)
+                        nfa['final_states'].append(new_state)
                     else:
                         new_state = f'p{cnt}'
                         cnt += 1
