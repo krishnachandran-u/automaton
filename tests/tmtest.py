fa995f326a97fdd3886da8c292d7b8185be8c04b
Krishnachandran U <kriss52450@gmail.com>
2024-11-25 21:36:48 +0530

    test: tmtest

---
+++ tests/tmtest.py
@@ -0,0 +1,19 @@
+from pykleene.tm import TM
+from typing import Dict
+from config import INPUTPATH, OUTPUTDIR
+import json
+from pprint import pprint
+
+FILENAME = 'tm.json'
+
+if __name__ == '__main__':
+    TMs: Dict[str, TM] = {}
+    with open(INPUTPATH(FILENAME), 'r') as file:
+        TMs = json.load(file)
+        # pprint(TMs)
+
+    for tmName, tmData in TMs.items():
+        tm = TM()
+        tm.loadFromJSONDict(tmData)
+        tm.image(dir=OUTPUTDIR(), save=True)
+        print(tm.accepts('1111#11111', verbose=False))
